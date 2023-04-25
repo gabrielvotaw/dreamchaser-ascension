94265cbe36e23cc1191bf75b380334b5df41d2be
Brice Truong <Bricetruong67@gmail.com>
2023-04-25 13:41:59 -0500

    Respawning for player

--- Assets/Scripts/Player Action/PlayerHPManager.cs
+++ Assets/Scripts/Player Action/PlayerHPManager.cs
@@ -11,13 +11,19 @@ public class PlayerHPManager : MonoBehaviour
     public float KnockBackUp = 30;
     public int immune = 1;
     private Rigidbody2D playerRB;
+
+    // Player Respawning
+    private PlayerRespawn playerRespawn;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        
         playerRB = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+
+        //player respawn
+        playerRespawn = GameObject.Find("Player").GetComponent<PlayerRespawn>();
     }
 
     // Update is called once per frame
@@ -25,6 +31,11 @@ public class PlayerHPManager : MonoBehaviour
     {
         if(currentHealth <= 0){
             gameObject.SetActive(false);
+            currentHealth = maxHealth;
+            gameObject.SetActive(true);
+            StartCoroutine(VisualIndicator(Color.white));
+            playerRespawn.RespawnNow();
+
         }
         if(currentHealth > maxHealth){
             currentHealth = maxHealth;
