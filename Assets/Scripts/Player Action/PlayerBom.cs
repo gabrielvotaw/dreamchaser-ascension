d3e999420f983ef69858c165295bc9c7de6947f8
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2023-04-26 23:36:11 -0500

    Player Weapon
    Not finished yer

---
+++ Assets/Scripts/Player Action/PlayerBom.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBom : MonoBehaviour
+{
+    public int contactDamage;
+    public int damageGive;
+    public GameObject playerFace;
+    public float KnockDirect = 1;
+    
+    public float playerKnockBackForce;
+    public float playerKnockBackUp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D entity) {
+      Debug.Log("Damage touched");
+      if (entity.gameObject.CompareTag("Player")){
+            entity.gameObject.GetComponent<PlayerHPManager>().damagePlayer(contactDamage);
+            entity.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerKnockBackForce*KnockDirect, playerKnockBackUp);
+
+      }
+
+      if (entity.tag == "Enemy"){
+			if(entity.tag == "Enemy" && entity.gameObject.GetComponent<EnemyLife>().blockBullet() == false){
+				entity.gameObject.GetComponent<EnemyLife>().HurtEnemy(damageGive);
+				
+			}
+			if(entity.tag == "Enemy" && entity.gameObject.GetComponent<EnemyLife>().blockBullet() == true){
+				entity.gameObject.GetComponent<EnemyLife>().shieldCrack();
+			}
+			
+		} 
+    }
+    
+    
+}
